4de7195554e6ed64af210d3f70070d93547a0977
Eugenia Galantseva <lucyatsky@gmail.com>
2020-02-13 14:36:53 -0600

    added menu items and directory

---
+++ src/components/menu-item/menu-item-component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './menu-item-style.scss';
+
+const MenuItem = ({title, imageUrl, size}) => (
+    <div className={`${size} menu-item`} style={{backgroundImage: `url(${imageUrl})`}}>
+    <div className='content'>
+      <div className='title'>{title}</div>
+      <span className='subtitle'>SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
