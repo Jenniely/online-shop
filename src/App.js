48d62d543f00720e5dbd590c0cdbc3ef4f066577
Eugenia Galantseva <lucyatsky@gmail.com>
2020-02-26 10:17:19 -0600

    refactored, added redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 
 import HomePage from './pages/homepage/homepage-component.jsx';
@@ -46,12 +46,16 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
-          <Route path='/signin' component={SignInPage}/>
+          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInPage/>)}/>
         </Switch>
       </div>
     );
   }
 }
+
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+});
 
 const mapDispatchToProps = dispatch => (
   {
@@ -59,4 +63,4 @@ const mapDispatchToProps = dispatch => (
   }
 );
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
