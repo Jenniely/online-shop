00d91ea3aa8cd5c28947293ee7d34351750aedc4
Eugenia Galantseva <lucyatsky@gmail.com>
2020-03-06 11:10:07 -0600

    updated selector for collections

--- src/components/collections-overview/collections-overview-component.jsx
+++ src/components/collections-overview/collections-overview-component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
-import {selectCollections} from '../../redux/shop/shop-selector.js';
+import {selectCollectionsForPreview} from '../../redux/shop/shop-selector.js';
 
 import CollectionPreview from '../collection-preview/collection-preview-component.jsx';
 
@@ -19,7 +19,7 @@ const CollectionsOverview =({collections}) => (
 
 const mapStateToProps = createStructuredSelector(
     {
-        collections: selectCollections
+        collections: selectCollectionsForPreview
     }
 );
 
